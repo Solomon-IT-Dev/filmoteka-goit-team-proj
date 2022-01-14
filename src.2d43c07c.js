350709ae3fdbccd6afa120beb2695382df10bd93
Solomon-IT-Dev <86610250+Solomon-IT-Dev@users.noreply.github.com>
2022-01-14 16:49:14 +0000

    Deploying to gh-pages from @ Solomon-IT-Dev/filmoteka-goit-team-proj@113f7546e67def5b78aaf51e45d7069baefdd4aa 🚀

--- src.2d43c07c.js
+++ src.2d43c07c.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/modal");
 },{"./sass/main.scss":"clu1","./js/modal":"RSqK"}]},{},["Focm"], null)
-//# sourceMappingURL=/filmoteka-goit-team-proj/src.b9dc87a8.js.map
+//# sourceMappingURL=/filmoteka-goit-team-proj/src.2d43c07c.js.map
