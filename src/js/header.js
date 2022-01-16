0462dbaeaa24064a39ed751168d2c2113bd666db
Igor Napolov <isnapolov@gmail.com>
2022-01-16 18:37:36 +0200

    js for header
    добавил переключение между красным и черным header

---
+++ src/js/header.js
@@ -0,0 +1,34 @@
+const buttonHome = document.querySelector(".button-home");
+
+const addBlackHeader = () => {
+    
+    document.querySelector('.header-container').classList.remove('header-container-red');
+    document.querySelector('.header-container').classList.add('header-container-black');
+
+    document.querySelector(".button-mylibrary").classList.remove('current');
+    document.querySelector(".button-home").classList.add('current');
+
+document.querySelector(".header-form").classList.remove('visually-hidden');
+    document.querySelector(".library-buttons").classList.add('visually-hidden');
+};
+
+buttonHome.addEventListener("click", addBlackHeader);
+
+
+
+const buttonMyLibrary = document.querySelector(".button-mylibrary");
+
+const addRedHeader = () => {
+  
+    document.querySelector('.header-container').classList.remove('header-container-black');
+    document.querySelector('.header-container').classList.add('header-container-red');
+
+    document.querySelector(".button-home").classList.remove('current');
+    document.querySelector(".button-mylibrary").classList.add('current');
+
+    document.querySelector(".library-buttons").classList.remove('visually-hidden');
+    document.querySelector(".header-form").classList.add('visually-hidden');
+
+};
+
+buttonMyLibrary.addEventListener("click", addRedHeader);
